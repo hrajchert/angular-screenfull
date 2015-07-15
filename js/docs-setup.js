4ebc15ecbb535c23c3c2de07f71a40be89a6701b
Hernan Rajchert <hrajchert@gmail.com>
2015-07-15 15:24:27 -0300

    Initial doc page

---
+++ js/docs-setup.js
@@ -0,0 +1,97 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "angularScreenfull",
+      "type": "overview",
+      "moduleName": "angularScreenfull",
+      "shortDescription": "Lalala some text, some other text, moar text",
+      "keywords": "angular angularscreenfull api app bla element fullscreen fullscreenctrl html js lalala moar module myapp ng-click ngsf-fullscreen ngsf-toggle-fullscreen overview text togglefullscreen"
+    },
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "Api Description",
+      "type": "overview",
+      "moduleName": "Api Description",
+      "shortDescription": "Angular Screenfull",
+      "keywords": "achieve allows angular angular-animate angular-screenfull angularscreenfull api app based bower bower_components clicked controller dependency description directive directives dist download element elements enable enter example examples exists exit export expose files folder form fullscreen fullscreenctrl fullscren functionality generated going hide html html5 import indicates install js library living marked min module multiple myapp needed ng-click ngsf-fullscreen ngsf-toggle-fullscreen note overview parent provide reason require result screenfull show-if-fullscreen show-if-fullscreen-enabled side simplest src status toggle togglefullscreen trough wrapper"
+    },
+    {
+      "section": "api",
+      "id": "angularScreenfull.directive:ngsfFullscreen",
+      "shortName": "ngsfFullscreen",
+      "type": "directive",
+      "moduleName": "angularScreenfull",
+      "shortDescription": "Marks the element that is going to be fullscreen",
+      "keywords": "angular angularscreenfull api app controller directive element expression fullscreen going html js marks module myapp ngsf-fullscreen ngsf-toggle-fullscreen ngsffullscreen optional store"
+    },
+    {
+      "section": "api",
+      "id": "angularScreenfull.directive:ngsfToggleFullscreen",
+      "shortName": "ngsfToggleFullscreen",
+      "type": "directive",
+      "moduleName": "angularScreenfull",
+      "shortDescription": "Adds a click handler to the element that toggles the nearest ngsf-fullscreen element",
+      "keywords": "adds angularscreenfull api click directive element handler nearest ngsf-fullscreen toggles"
+    },
+    {
+      "section": "api",
+      "id": "angularScreenfull.directive:showIfFullscreen",
+      "shortName": "showIfFullscreen",
+      "type": "directive",
+      "moduleName": "angularScreenfull",
+      "shortDescription": "Shows or hides the element (using ng-hide) if the closest",
+      "keywords": "angularscreenfull api behaviour change closest default defaults directive element false fullscreen hides inverts mode ng-hide ngsf-fullscreen parent passing showiffullscreen true"
+    },
+    {
+      "section": "api",
+      "id": "angularScreenfull.directive:showIfFullscreenEnabled",
+      "shortName": "showIfFullscreenEnabled",
+      "type": "directive",
+      "moduleName": "angularScreenfull",
+      "shortDescription": "Shows or hides the element (using ng-hide) if the browser has fullscreen",
+      "keywords": "angularscreenfull api browser capabilities directive element fullscreen hides ng-hide"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/dist/angular-screenfull.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/screenfull.js/2.0.0/screenfull.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "js/dist/angular-screenfull.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/screenfull.js/2.0.0/screenfull.js"
+  ]
+};
